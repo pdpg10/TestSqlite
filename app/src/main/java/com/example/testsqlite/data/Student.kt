c2422575c6ee22a8b711839748569f730d133ed9
Akhrorkhoja <akhrorkhoja@mysuperdispatch.com>
2019-08-22 18:56:15 +0500

    version simple sqlite

--- app/src/main/java/com/example/testsqlite/data/Student.kt
+++ app/src/main/java/com/example/testsqlite/data/Student.kt
@@ -1,13 +1,9 @@
 package com.example.testsqlite.data
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 
-@Entity
 data class Student(
     val name: String = "",
     val age: Int = 16,
     val course: Int = 1,
-    @PrimaryKey(autoGenerate = true)
     val id: Long = 0
 )
